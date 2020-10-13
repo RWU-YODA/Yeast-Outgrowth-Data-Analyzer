51e63d4a501d60e2c0ba2cf7d69a59208f139079
Matthew Ruch <mruch996@g.rwu.edu>
2020-10-13 12:48:49 -0400

    1/2 tuesdays work

--- angular-YODA/src/app/app-routing.module.ts
+++ angular-YODA/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'about', component: AboutComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
