Fixed calculation of survival data
fixed series (was just taking first 6, now we needed to take every other starting at i = 0)
added about page

analyzer/DayData.py
@@ -55,23 +55,35 @@ class DayData:
             return 100000000
         # Get the points braketing 0.3 OD
         lowerIndex = wellVector.where(wellVector < ODT).idxmax()
+        #print("lower Index: ", lowerIndex)
         upperIndex = wellVector.where(wellVector > ODT).idxmin()
+        #print("upper Index: ", upperIndex)
 
 
         timeRange = timeVector[lowerIndex:upperIndex+1].values
+        #print("timeRange", timeRange)
         odRange = np.log(wellVector[lowerIndex:upperIndex+1].values)
+        #print("odRange: ", odRange)
+        
+        #odRange = abs(odRange)
+        #print("odRange: ", odRange)
 
         odRange = odRange.reshape(-1,1)
+        #print("odRange: ", odRange)
+
+        #print("ODT", ODT)
+        #print("log ODT", np.log(ODT))
+        #ODT = np.array(ODT)
 
         lm = LinearRegression()
         lm.fit(odRange, timeRange)
-        #ax = df.plot(x=feature, y=target, kind='scatter', alpha=0.5, logy=True)
-        #X = df[[feature]]
-        #y = np.log(df[target]) # Apply natural log function to the target
-        #y_pred = np.exp(model.predict(X)) # Apply exponential function (inverse of natural log) to the predictions
-        timePrediction = lm.predict(odRange)
+        
+        # this line of code is dumb but seems to work, Ill run with it
+        timePrediction = lm.predict(np.array([np.log(ODT), np.log(ODT)]).reshape(-1,1))
+        #print(timePrediction)
         timePrediction = timePrediction.transpose()
         return timePrediction
+        
 
     @classmethod
     def calculateDoublingTime(cls, wellVector, timeVector):

analyzer/Experiment.py
@@ -8,14 +8,18 @@ class Experiment:
     def __init__(self, dayList):
         # dayList is an array of DayData objects
         self.dayList = dayList
+        #print("Daylist: ", self.dayList)
 
     def calculateThresholdMatrix(self,ODT):
         # Get the names of the wells
         names = list(self.dayList[0].getThresholdTimes(ODT))
         # Create a list of all the threshold times. Note this is a numpy array
         thresholdList = [day.getThresholdTimes(ODT).values for day in self.dayList]
+        #print("ThresholdList: ", thresholdList)
         # Return a new pandas dataframe with the names
-        return pd.DataFrame(np.concatenate(thresholdList),columns=names)
+        threshold_df = pd.DataFrame(np.concatenate(thresholdList),columns=names)
+        #print(threshold_df)
+        return threshold_df
 
     # Get the doubling times of the first day (generally day 2)
     def generateFirstDayDoublingTimes(self):
@@ -40,9 +44,6 @@ class Experiment:
     def generateGroupedSurvivalMatrix(self,nameList,dayNameList,ODT):
         # Generate the standard survivalMatrix for each well.
         wellSurvivalMatrix = self.generateWellSurivalMatrix(ODT)
-        print("FSJKNFBKJSN generateGroupedSurvivalMatrix")
-        print(dayNameList)
-        print(nameList)
         #testList = [2,4]
         # Rename the rows with the appropriate days
         groupedSurvivalMatrix = wellSurvivalMatrix#.rename(index= lambda x:  dayNameList[x] )
@@ -54,9 +55,6 @@ class Experiment:
         return averagedOutput
 
     def generateGroupedSurvivalMatrixSDs(self,nameList,dayNameList,ODT):
-        print("FSJKNFBKJSN generateGroupedSurvivalMatrixSDs")
-        print(dayNameList)
-        print(nameList)
         # Generate the standard survivalMatrix for each well.
         wellSurvivalMatrix = self.generateWellSurvivalMatrix(ODT)
         # Rename the rows with the appropriate days
@@ -91,8 +89,21 @@ class Experiment:
 
     @classmethod
     def computeSurvivalValues(cls,thresholdTimes):
+        #print("thresholdTimes: computeSurvivalValues: ", thresholdTimes)
         # Subtracting each timepoint's vector from the initial value
         thresholdDifferences = thresholdTimes[0:len(thresholdTimes)] - thresholdTimes[0]
         doublingTime = thresholdTimes.iloc[-1]
+        '''
+        print("****************************")
+        print("Doubling Time")
+        print(doublingTime)
+        print("*******************************")
+        print("Threshold Diff")
+        print(thresholdDifferences)
+        print("********************************")
+        '''
         survivalVector = (1/ (2**(thresholdDifferences.values/doublingTime))) * 100
+        #print("************************")
+        #print("Survival Vector")
+        #print(survivalVector)
         return survivalVector